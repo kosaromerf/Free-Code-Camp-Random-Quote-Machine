a1f69897b49c773119a354fce37b548254a85247
kosaromerf <kosaromerf@gmail.com>
2024-04-28 02:14:06 +0300

    Add : random color change with button click

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Button, Card, Container, CardText } from "react-bootstrap";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState({
+    backgroundColor: "rgb(255,255,255)",
+    color: "rgb(-255,-255,-255)",
+  });
+
+  const getRandom = () => {
+    return Math.floor(Math.random() * 255 + 1);
+  };
+  const changeColor = () => {
+    let r = 100;
+    let g = 100;
+    let b = 100;
+    while (r + g + b < 512 && r + g + b > 256) {
+      r = getRandom();
+      g = getRandom();
+      b = getRandom();
+    }
+    setColor({
+      backgroundColor: `rgb(${r},${g},${b})`,
+      color: `${r + g + b < 256 ? "white" : "black"}`,
+    });
+  };
+
+  console.log(color);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container className="d-flex justify-content-center align-items-center full">
+      <Card id="quote-box" className="d-flex" style={color}>
+        <CardText id="text" style={color}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis fugit
+          dignissimos corrupti fuga ipsa debitis perferendis omnis?
+        </CardText>
+        <CardText id="author">
+          <em>- sdasdasdasd</em>
+        </CardText>
+        <div className="d-flex  justify-content-between">
+          <Button
+            id="new-quote"
+            variant="primary"
+            onClick={() => changeColor()}
+          >
+            New Quote
+          </Button>
+          <a id="tweet-quote" href="#">
+            Share
+          </a>
+        </div>
+      </Card>
+    </Container>
+  );
 }
 
-export default App
+export default App;
